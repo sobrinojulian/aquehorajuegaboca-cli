423854223819c0ad445d5fd357417762f9aeca04
Sobrino Julian <sobrinojulian@protonmail.com>
2019-11-26 17:27:55 -0300

    push esta mierda

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-const https = require("http");
+const http = require("http");
 const cheerio = require("cheerio");
 
 const main = () => {
     const url = "http://www.aquehorajuegaboca.com.ar/";
-    https.get(url, res => {
+    http.get(url, res => {
         let data = "";
         res.on("data", chunk => (data += chunk));
         res.on("end", () => {
@@ -18,4 +18,32 @@ const main = () => {
         });
     });
 };
-main();
+
+const main2 = () => {
+    const url = "http://www.aquehorajuegaboca.com.ar/proximos";
+    http.get(url, res => {
+        let data = "";
+        res.on("data", chunk => (data += chunk));
+        res.on("end", () => {
+            const $ = cheerio.load(data);
+
+            //const children = $('#maincontainer').children()[1]
+            //const foo = $(".equipo_proximo", "children")
+            //console.log(children)
+            const ch = $('#maincontainer').children()
+            const a = $(ch).slice(1)
+            const partidos = $(a).map(function(i, el) {
+                    const equipos = $(".equipo_proximo", el).map(function(i, el) {
+                        const equipo = $(el).text()
+                        console.log(i, equipo)
+                        return el
+
+                    })
+
+                    //return $(this).text();
+                }).get()
+                //console.log(partidos)
+        });
+    });
+};
+main2();
