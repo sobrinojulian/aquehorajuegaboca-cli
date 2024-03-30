2e8035abf9c15ddbd6fa5e10d702dab88836dcee
Sobrino Julian <sobrinojulian@protonmail.com>
2024-03-30 17:11:28 -0300

    *

--- cli.js
+++ cli.js
@@ -1,53 +1,28 @@
-#!/usr/bin/env node
-
 import { load } from 'cheerio'
 
-const main = async () => {
-  const url = 'https://aquehorajuega.co/equipos/a-que-hora-juega-boca-juniors/'
-  const response = await fetch(url)
-  const html = await response.text()
-  const $ = load(html)
-  const divs = getEventsDivs($)
-  for (const event of divs) {
-    const data = parse($, event)
-    const formattedEvent = format(data)
-    console.log(formattedEvent)
+async function fetchData() {
+  try {
+    const response = await fetch('https://www.espn.com.ar/futbol/equipo/calendario?id=5')
+    const html = await response.text()
+    const $ = load(html)
+
+    // Extract the script content
+    const scriptContent = $('script')
+      .filter(function () {
+        return $(this).html().includes('__espnfitt__')
+      })
+      .html()
+
+    // Extract the page object from the script content
+    const pageObject = JSON.parse(scriptContent.match(/window\['__espnfitt__'\]\s*=\s*({.*?});/)[1])
+
+    // Extract the desired data from the page object
+    const desiredData = pageObject.page.content.fixtures.events
+
+    console.log(desiredData)
+  } catch (error) {
+    console.error('Error fetching data:', error)
   }
 }
 
-const getEventsDivs = ($) => {
-  return $('[itemprop="startDate"]')
-    .map((_, el) => $(el).parent().parent())
-    .get()
-}
-
-const parse = ($, event) => {
-  const date = $('[itemprop="startDate"]', event).text().split('\n')[1].trim().split('  ').join(' ')
-  const city = $('[itemprop="address"]', event).text().trim()
-  const stadium = $('[itemprop="name"]', event).text().trim().split('\n')[0]
-  const match = $('.row[itemprop="name"]', event).attr('content').replace('Vs.', 'vs')
-  const tournament = $('span[itemprop="description"]', event).text().trim().replace('Torneo: ', '')
-
-  return {
-    date,
-    city,
-    stadium,
-    match,
-    tournament,
-  }
-}
-
-const format = (data) => {
-  const { date, city, stadium, match, tournament } = data
-
-  // prettier-ignore
-  return [
-    `🗓️  ${date}`,
-    `⚽️ ${match}`,
-    `🏟️  ${stadium} (${city})`,
-    `🏆 ${tournament}`,
-    '',
-  ].join('\n');
-}
-
-main()
+fetchData()
